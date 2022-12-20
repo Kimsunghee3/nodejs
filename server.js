0fa0c760f356342a5bd49e73c4152213c1a5b0e3
cloudcoke <lms4296@gmail.com>
2022-12-20 16:00:15 +0900

    feat: server.js 초기 설정

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const router = require("./routes");
+const nunjucks = require("nunjucks");
 
-app.get("/", (req, res)=> {
-    res.send('/page');
-})
-
-app.listen(3000, ()=> {
-    console.log(`server Start`)
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
 });
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+
+app.get("/", (req, res) => {
+  res.send("/page");
+});
+
+app.listen(3000, () => {
+  console.log(`server Start`);
+});
